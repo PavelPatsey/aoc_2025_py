dd3d98f2ba944dbe95e67b98dbc494b6e3b47ed3
Pavel Patsey <patsey.pavel@gmail.com>
2025-12-01 08:31:43 +0300

    pass test input

--- 01/01.py
+++ 01/01.py
@@ -17,11 +17,29 @@ def get_answer(data):
     return res
 
 
+def get_answer_2(data):
+    x = 50
+    res = 0
+    div = 0
+    for command in data:
+        if command[0] == "L":
+            x = x - command[1]
+        else:
+            x = x + command[1]
+        new_div = x // 100
+        res += abs(new_div - div)
+        x = x % 100
+        div = x // 100
+        # print(x, div)
+    return res
+
+
 def main():
-    file = "input.txt"
+    file = "test_input.txt"
     data = get_data(file)
     print(data)
-    print(get_answer(data))
+    # print(get_answer(data))
+    print(get_answer_2(data))
 
 
 if __name__ == "__main__":
