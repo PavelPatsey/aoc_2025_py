f07bea336fd4db712518f43fc7270e8999e375bd
Pavel Patsey <patsey.pavel@gmail.com>
2025-12-02 09:07:23 +0300

    solve day 2 part 2

--- 02/02.py
+++ 02/02.py
@@ -25,21 +25,54 @@ def get_answer(data):
     return res
 
 
+def is_invalid_2(n: int) -> bool:
+    s = str(n)
+    if s.startswith("0"):
+        return False
+    n = len(s)
+    for i in range(2, len(s) + 1):
+        if n % i != 0:
+            continue
+        a = s[0 : n // i]
+        if a * i == s:
+            return True
+    return False
+
+
+def get_answer_2(data):
+    res = 0
+    for start, end in data:
+        for i in range(start, end + 1):
+            if is_invalid_2(i):
+                res += i
+    return res
+
+
 def main():
     file = "input.txt"
     data = get_data(file)
     print(get_answer(data))
+    print(get_answer_2(data))
 
 
 if __name__ == "__main__":
     main()
 
-    # assert is_invalid(11) == True
-    # assert is_invalid(22) == True
-    # assert is_invalid(1188511885) == True
+    assert is_invalid(11) == True
+    assert is_invalid(22) == True
+    assert is_invalid(1188511885) == True
     assert is_invalid(1010) == True
 
     assert is_invalid(123) == False
     assert is_invalid(12) == False
     assert is_invalid(100) == False
     assert is_invalid(101) == False
+
+    assert is_invalid_2(11) == True
+    assert is_invalid_2(22) == True
+    assert is_invalid_2(2121212121) == True
+    assert is_invalid_2(999) == True
+    assert is_invalid_2(1010) == True
+    assert is_invalid_2(1188511885) == True
+
+    assert is_invalid_2(1726462) == False
