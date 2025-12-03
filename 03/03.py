f1c5ceedc028cfe0fa4af090911355063f4e293a
Pavel Patsey <patsey.pavel@gmail.com>
2025-12-03 08:15:10 +0300

    solve day 3 part 1

--- 03/03.py
+++ 03/03.py
@@ -1,17 +1,29 @@
 def get_data(input_file):
     with open(input_file, "r") as file:
         data = file.read().splitlines()
-    return data
+    return [int(line) for line in data]
+
+
+def get_max_joltage(n: int) -> int:
+    digits = [int(char) for char in str(n)]
+    res = 0
+    for i in range(len(digits)):
+        for j in range(i + 1, len(digits)):
+            a = int(digits[i] * 10 + digits[j])
+            res = max(res, a)
+    return res
 
 
 def get_answer(data):
-    return
+    res = 0
+    for n in data:
+        res += get_max_joltage(n)
+    return res
 
 
 def main():
-    file = "test_input.txt"
+    file = "input.txt"
     data = get_data(file)
-    print(data)
     print(get_answer(data))
 
 
