4e6e954dba667b836a5bbbaadb596224325aba78
Pavel Patsey <patsey.pavel@gmail.com>
2025-12-05 08:45:13 +0300

    solve day 5 part 2

--- 05/05.py
+++ 05/05.py
@@ -1,3 +1,6 @@
+from operator import itemgetter
+
+
 def get_data(input_file):
     with open(input_file, "r") as file:
         data = file.read().splitlines()
@@ -26,10 +29,43 @@ def get_answer(ranges, nums):
     return counter
 
 
+def normalize(merged: list) -> list:
+    if len(merged) < 2:
+        return merged
+    while len(merged) >= 2:
+        a1, b1 = merged.pop()
+        a2, b2 = merged.pop()
+        if a1 <= b2:
+            min_a = min(a1, a2)
+            max_b = max(b1, b2)
+            merged.append((min_a, max_b))
+        else:
+            merged.append((a2, b2))
+            merged.append((a1, b1))
+            break
+    return merged
+
+
+def get_answer_2(ranges):
+    ranges = sorted(ranges, key=itemgetter(1))
+    print(ranges)
+    merged = []
+    for a, b in ranges:
+        merged.append((a, b))
+        merged = normalize(merged)
+    print(merged)
+
+    res = 0
+    for a, b in merged:
+        res += b - a + 1
+    return res
+
+
 def main():
     file = "input.txt"
     ranges, nums = get_data(file)
     print(get_answer(ranges, nums))
+    print(get_answer_2(ranges))
 
 
 if __name__ == "__main__":
