c1f570c136b842cb62e0407bb995289d911923b7
Pavel Patsey <patsey.pavel@gmail.com>
2025-12-06 14:10:06 +0300

    refactor

--- 06/06_p1.py
+++ 06/06_p1.py
@@ -1,54 +1,37 @@
-from functools import reduce
 from math import prod
 
 
 def get_data(input_file):
     with open(input_file, "r") as file:
         data = file.read().splitlines()
-    return [
-        [int(x) if x not in {"*", "+"} else x for x in line.split()] for line in data
-    ]
+    return [line.split() for line in data]
 
 
-def transpose(matrix):
-    rows = len(matrix)
-    cols = len(matrix[0])
-
-    transposed_matrix = [[0 for _ in range(rows)] for _ in range(cols)]
-
-    for i in range(rows):
-        for j in range(cols):
-            transposed_matrix[j][i] = matrix[i][j]
-    return transposed_matrix
+def calculate(nums: list, op: str) -> int:
+    if op == "+":
+        return sum(nums)
+    return prod(nums)
 
 
 def get_answer(grid):
     rows = len(grid)
     cols = len(grid[0])
     res = 0
-    for row in grid:
-        print(row)
-        if row[-1] == "+":
-            # a = sum(row[: rows - 1])
-            numbers = [x for x in row if isinstance(x, int)]
-            a = sum(numbers)
-            res += a
-        else:
-
-            # b = reduce(lambda acc, x: acc * x, row[: rows - 1], 1)
-            numbers = [x for x in row if isinstance(x, int)]
-            b = reduce(lambda acc, x: acc * x, numbers, 1)
-            res += b
+    for c in range(cols):
+        nums = []
+        for r in range(rows):
+            char = grid[r][c]
+            if char in {"+", "*"}:
+                res += calculate(nums, char)
+            else:
+                nums.append(int(char))
     return res
 
 
 def main():
     file = "input.txt"
     data = get_data(file)
-    print(data)
-    grid = transpose(data)
-    print(grid)
-    print(get_answer(grid))
+    print(get_answer(data))
 
 
 if __name__ == "__main__":
