a78ba1725eb1a83b8ed8903a3d6bf0d96deba5dc
Pavel Patsey <patsey.pavel@gmail.com>
2025-12-08 09:46:00 +0300

    fix day 2

--- 08/08_d2.py
+++ 08/08_d2.py
@@ -72,14 +72,35 @@ def get_answer_2(points):
     sorted_ds_points = get_sorted_ds_points(points, distances)
     max_len = -1
     n = 0
-    graph = defaultdict(set)
+    groups = []
     while max_len < len(points):
         _d, indxs = sorted_ds_points[n]
         i, j = indxs
-        graph[i].add(j)
-        graph[j].add(i)
 
-        groups = make_groups(graph)
+        found_i = set()
+        found_j = set()
+        for gr in groups:
+            if i in gr:
+                found_i = gr
+            if j in gr:
+                found_j = gr
+        if not found_i and not found_j:
+            groups.append({i, j})
+        elif found_i and found_j:
+            if found_i == found_j:
+                groups.remove(found_i)
+            else:
+                groups.remove(found_i)
+                groups.remove(found_j)
+            groups.append(found_i | found_j)
+        elif found_i:
+            groups.remove(found_i)
+            groups.append(found_i | {j})
+        elif found_j:
+            groups.remove(found_j)
+            groups.append(found_j | {i})
+        else:
+            assert False, "error case!"
         lens = list(map(lambda x: len(x), groups))
         max_len = max(lens)
         print(f"{max_len=}")
