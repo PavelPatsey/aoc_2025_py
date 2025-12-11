97a86761b5a2badf591946a0b869dc11f31b6205
Pavel Patsey <patsey.pavel@gmail.com>
2025-12-11 09:43:43 +0300

    merge solutions

--- 11/11.py
+++ 11/11.py
@@ -26,11 +26,38 @@ def get_answer(graph):
     return res
 
 
+def get_answer_2(graph):
+    memo = {}
+
+    def dfs(node, vis_state):
+        key = (node, vis_state)
+        if key in memo:
+            return memo[key]
+
+        dac_visited, fft_visited = vis_state
+        paths = 0
+
+        if node == "out":
+            if dac_visited and fft_visited:
+                paths = 1
+        else:
+            for v in graph.get(node, []):
+                new_dac = dac_visited or (v == "dac")
+                new_fft = fft_visited or (v == "fft")
+                new_state = (new_dac, new_fft)
+                paths += dfs(v, new_state)
+
+        memo[key] = paths
+        return paths
+
+    return dfs("svr", (False, False))
+
+
 def main():
     file = "input.txt"
     graph = get_data(file)
-    print(graph)
     print(get_answer(graph))
+    print(get_answer_2(graph))
 
 
 if __name__ == "__main__":
